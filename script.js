03de4b90230c5b285a13d1bc0efbc9f9d7e4b383
Maxwell <lubalecliff@gmail.com>
2023-10-28 20:11:09 +0300

    Added a html ,js and css code

--- script.js
+++ script.js
@@ -1,71 +1,58 @@
-// Define global variables to store movie data
-let movies = []; // To store a list of movies
-let selectedMovie = 1; // To store the currently selected movie
+const filmsList = document.getElementById('films');
+const movieDetails = document.getElementById('movie-details');
+const buyTicketButton = document.getElementById('buy-ticket');
+// let selectedFilm = null; // Store the selected film globally
 
-// Function to fetch movie data from the JSON server
-async function fetchMovies() {
-  try {
-    const response = await fetch('http://localhost:3000/films'); // Use the appropriate API endpoint
-    const data = await response.json();
-    movies = data;
-    // Populate the movie list in the UI
-    populateMovieList();
-    // Display the details of the first movie
-    if (movies.length > 0) {
-      selectedMovie = movies[0];
-      updateMovieDetails();
-    }
-  } catch (error) {
-    console.error('Error fetching movie data:', error);
-  }
-}
+// Fetch the list of movies from the server
+fetch(' http://localhost:3000/films')
+  .then((response) => response.json())
+  .then((data) => {
+    // Populate the movie list
+    data.forEach((film) => {
+      const filmItem = document.createElement('li');
+      filmItem.textContent = film.title;
+      filmItem.classList.add('film', 'item');
+      filmItem.dataset.id = film.id;
+      filmsList.appendChild(filmItem);
 
-// Function to populate the movie list in the UI
-function populateMovieList() {
-  const movieList = document.getElementById('films'); // Get the UL element
-  movieList.innerHTML = ''; // Clear the existing list
-
-  movies.forEach((movie) => {
-    const listItem = document.createElement('li');
-    listItem.textContent = movie.title;
-    listItem.classList.add('film', 'item');
-
-    // Add an event listener to select the movie when clicked
-    listItem.addEventListener('click', () => {
-      selectedMovie = movie;
-      updateMovieDetails();
+      // Add click event to each film item
+      filmItem.addEventListener('click', () => {
+        selectedFilm = film;
+        displayMovieDetails(film);
+      });
     });
 
-    movieList.appendChild(listItem);
+    // Initial movie details
+    displayMovieDetails(data[0]);
   });
-}
 
-// Function to update the movie details in the UI
-function updateMovieDetails() {
-  const movieDetails = document.getElementById('movie-details'); // Get the movie details section
-  // Update the movie details in the UI based on the selectedMovie object
+// Display movie details
+function displayMovieDetails(film) {
   movieDetails.innerHTML = `
-    <img src="${selectedMovie.poster}" alt="${selectedMovie.title} Poster">
-    <h2>${selectedMovie.title}</h2>
-    <p>Runtime: ${selectedMovie.runtime} minutes</p>
-    <p>Showtime: ${selectedMovie.showtime}</p>
-    <p>Available Tickets: ${selectedMovie.capacity - selectedMovie.tickets_sold}</p>
-    <button id="buy-ticket" onclick="buyTicket()">Buy Ticket</button>
+    <h3>${film.title}</h3>
+    <p>Runtime: ${film.runtime} minutes</p>
+    <p>Showtime: ${film.showtime}</p>
+    <p id="available-tickets">Available Tickets: ${film.capacity - film.tickets_sold}</p>
+    <p>Description: ${film.description}</p>
+    <img src="${film.poster}" alt="${film.title} Poster" />
   `;
-}
 
-// Function to handle buying tickets
-function buyTicket() {
-  if (selectedMovie && selectedMovie.capacity > selectedMovie.tickets_sold) {
-    // Update the UI to reflect the ticket purchase
-    selectedMovie.tickets_sold++;
-    updateMovieDetails();
+  if (film.capacity - film.tickets_sold === 0) {
+    buyTicketButton.textContent = 'Sold Out';
+    buyTicketButton.disabled = true;
   } else {
-    // Handle the case when the movie is sold out
-    document.getElementById('buy-ticket').textContent = 'Sold Out';
-    // You can also add a class to the movie list item to indicate it's sold out.
+    buyTicketButton.textContent = 'Buy Ticket';
+    buyTicketButton.disabled = false;
   }
 }
 
-// Initialize the application by fetching movie data
-fetchMovies();
+// Buy Ticket button click event
+buyTicketButton.addEventListener('click', () => {
+  if (selectedFilm && selectedFilm.capacity > selectedFilm.tickets_sold) {
+    // Subtract the available tickets by 1
+    selectedFilm.tickets_sold++;
+    // Update the available tickets count in the movie details
+    const availableTickets = selectedFilm.capacity - selectedFilm.tickets_sold;
+    document.getElementById('available-tickets').textContent = `Available Tickets: ${availableTickets}`;
+  }
+});
